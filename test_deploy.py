90470612d9bdd59de7802610f5a4829ea0f91b0f
AnnaCherleniak <114302635+AnnaCherleniak@users.noreply.github.com>
2023-10-05 18:18:05 +0700

    Update and rename deploy.py to test_deploy.py

---
+++ test_deploy.py
@@ -0,0 +1,33 @@
+from sshcheckers import ssh_checkout, upload_files
+import yaml
+import pytest
+
+with open('config.yaml') as f:
+    data = yaml.safe_load(f)
+
+
+class TestDeployPositive:
+
+    def test_step1_deploy(self):
+        res = []
+        upload_files(data["host"], data["user"], data["passwd"], data["local_path"], data["remote_path"])
+        res.append(ssh_checkout(data["host"], data["user"], data["passwd"],
+                                f"echo {data['passwd']} | sudo -S dpkg -i {data['remote_path']}", "Настраивается пакет"))
+        res.append(ssh_checkout(data["host"], data["user"], data["passwd"],
+                                f"echo {data['passwd']} | sudo -S dpkg -s p7zip-full", "Status: install ok installed"))
+        assert all(res)
+
+    def test_step2(self, clear_folders, make_files):
+        res = []
+        res.append(ssh_checkout(data["host"], data["user"], data["passwd"],
+                                f'cd {data["PATH_IN"]}; 7z a {data["PATH_OUT"]}/arh1', 'Everything is Ok'))
+        res.append(ssh_checkout(data["host"], data["user"], data["passwd"],
+                                f'cd {data["PATH_OUT"]}; 7z e arh1.7z -o{data["PATH_EXIT"]}', 'Everything is Ok'))
+        for item in make_files:
+            res.append(ssh_checkout(data["host"], data["user"], data["passwd"],
+                                    f'ls {data["PATH_EXIT"]}', item))
+        assert all(res), 'test_step2 FAIL'
+
+
+if __name__ == '__main__':
+    pytest.main(['-v'])
